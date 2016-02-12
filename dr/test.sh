353ffdab50f1541433abeeddea629b207475d112
oldlaptop <oldlaptop654@aol.com>
2016-02-11 23:12:29 -0500

    shellcheck pass

--- dr/test.sh
+++ dr/test.sh
@@ -1,17 +1,17 @@
 #! /bin/sh
 
-RUNS_PER_N=${RUNS_PER_N:-1000}
-DR_ARG=${DR_ARG:-d10}
+RUNS_PER_N="${RUNS_PER_N:-1000}"
+DR_ARG="${DR_ARG:-d10}"
 N_RANGE="1 2 3 4 5 6 7 8 9 10"
 
 for N in $N_RANGE
 do
 	RUN_NO=1
 	NUM_NS=0
-	while [ $RUN_NO -lt $RUNS_PER_N ]
+	while [ "$RUN_NO" -lt "$RUNS_PER_N" ]
 	do
-		DIE=$(./dr $DR_ARG)
-		if [ $DIE -eq $N ]
+		DIE=$(./dr "$DR_ARG")
+		if [ "$DIE" -eq "$N" ]
 		then
 			NUM_NS=$(( $NUM_NS + 1 ))
 		fi
